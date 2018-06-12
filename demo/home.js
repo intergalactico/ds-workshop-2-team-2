ea920b0c6835e38d6295c59eb4c1184ec44f9a8e
Tuomo Oila <tuomo.a.karkkainen@helsinki.fi>
2018-06-12 15:49:36 +0300

    Added a section

--- demo/home.js
+++ demo/home.js
@@ -22,6 +22,19 @@ const Home = () => {
             action="Event overview"
             background='/assets/dsconf-wide.jpg'
             onAction={() => console.log("Hello")}/>
+      <div>
+      <div>
+        <Section backgroundColor='black'>
+          <SectionHeader color='white'>A two-day conference with one day of keynotes & one day of workshops.</SectionHeader>
+          <SectionText color='white'>DSCONF aims to bring people together to exchange ideas, solutions, inspiration, and learn how to build digital products better and more efficiently with Design Systems.
+An event for digital product designers, builders, business owners and organizations who want to learn how to build design systems and why they are needed in the future. We are at a crossroads whereby making the right decisions, you will gain a significant business advantage and ensure the best possible user experience for your digital products and services.
+Sounds good? Make sure you familiarize yourself with our code of conduct.</SectionText>
+        </Section>
+      </div>
+        <Section>
+          <SectionHeader>Hello!</SectionHeader>
+          <SectionText>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</SectionText>
+        </Section>
         <BroughtToYouBy />
     </div>
   )
