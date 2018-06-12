14bd9e59ae4c52dfeeaf7183a1dce7c13ee40ffe
Tuomo Oila <tuomo.a.karkkainen@helsinki.fi>
2018-06-12 12:17:35 +0300

    Colors added

--- src/Colors/colorset.js
+++ src/Colors/colorset.js
@@ -2,5 +2,12 @@ export default {
   dark: '#353740',
   grey: '#747d88',
   darkblue: '#01092F',
-  lightgreen: '#79C8AF'
+  lightgreen: '#79C8AF',
+  blue: '1B8CEB',
+  green: '00D2B2',
+  red: 'DC1D5E',
+  lightGrey: '888888',
+  disabled: 'E0E0E1',
+  white: 'FFFFFF',
+  black: '0B0B0B'
 };
