5eabc5b80acb2214eef4ee9d1d78a78dff6ffdc6
Timo Koro <timo.koro@sc5.io>
2018-06-12 15:45:33 +0300

    Clean up unnecessary lorem ipsum

--- demo/home.js
+++ demo/home.js
@@ -4,13 +4,10 @@
  */
 
 import React from 'react'
-import ReactDOM from 'react-dom'
 
-import { Hero, Icon, Section, SectionHeader, SectionText, Color, Navigation, Button } from '../lib/my-awesome-library'
+import { Hero, Navigation, Button } from '../lib/my-awesome-library'
 
 import BroughtToYouBy from './BroughtToYouBy';
-
-const st = Section.SectionTypography
 
 const Home = () => {
   return (
@@ -25,13 +22,7 @@ const Home = () => {
             action="Event overview"
             background='/assets/dsconf-wide.jpg'
             onAction={() => console.log("Hello")}/>
-      <div>
-        <Section>
-          <SectionHeader>Hello!</SectionHeader>
-          <SectionText>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</SectionText>
-        </Section>
         <BroughtToYouBy />
-      </div>
     </div>
   )
 }
