249640ae8d668d0d495fc95d53623cc0d52090a9
Timo Koro <timo.koro@sc5.io>
2018-06-12 15:44:55 +0300

    Change BroughtToYouBy subtitle color to royalgold

--- demo/BroughtToYouBy.js
+++ demo/BroughtToYouBy.js
@@ -6,17 +6,17 @@ const BroughtToYouBy = () => {
 
   return (
     <Section backgroundColor={Color.darkblue}>
-      <SectionSubtitle color={Color.white} textAlign='center'>Main organizer</SectionSubtitle>
+      <SectionSubtitle color={Color.royalgold} textAlign='center'>Main organizer</SectionSubtitle>
       <div style={{display: 'flex', justifyContent: 'center'}}>
         <img src='./assets/intergalactico-dark.png' width={imgSize} height={imgSize}/>
       </div>
-      <SectionSubtitle color={Color.white} textAlign='center'>Partners</SectionSubtitle>
+      <SectionSubtitle color={Color.royalgold} textAlign='center'>Partners</SectionSubtitle>
       <div style={{display: 'flex', justifyContent: 'center'}}>
         <img src='./assets/abstract-dark.png' width={imgSize} height={imgSize}/>
         <img src='./assets/eficode-light.png' width={imgSize} height={imgSize}/>
         <img src='./assets/futurice-light.png' width={imgSize} height={imgSize}/>
       </div>
-      <SectionSubtitle color={Color.white} textAlign='center'>Supported by</SectionSubtitle>
+      <SectionSubtitle color={Color.royalgold} textAlign='center'>Supported by</SectionSubtitle>
       <div style={{display: 'flex', justifyContent: 'center'}}>
         <img src='./assets/idean-light.png' width={imgSize} height={imgSize}/>
         <img src='./assets/smartly-dark.png' width={imgSize} height={imgSize}/>
