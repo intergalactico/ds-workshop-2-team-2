cd3c5fc332b8f483a8bec4879c9ee24253b93d50
Timo Koro <timo.koro@sc5.io>
2018-06-12 15:31:46 +0300

    Add partner logos into BroughtToYouBy section

--- demo/BroughtToYouBy.js
+++ demo/BroughtToYouBy.js
@@ -2,12 +2,26 @@ import React from 'react';
 import { Section, SectionSubtitle, Color } from '../lib/my-awesome-library'
 
 const BroughtToYouBy = () => {
+  const imgSize = '35%';
+
   return (
     <Section backgroundColor={Color.darkblue}>
       <SectionSubtitle color={Color.white} textAlign='center'>Main organizer</SectionSubtitle>
-      <img src='./assets/intergalactico-dark.png' width='250px'/>
+      <div style={{display: 'flex', justifyContent: 'center'}}>
+        <img src='./assets/intergalactico-dark.png' width={imgSize} height={imgSize}/>
+      </div>
       <SectionSubtitle color={Color.white} textAlign='center'>Partners</SectionSubtitle>
+      <div style={{display: 'flex', justifyContent: 'center'}}>
+        <img src='./assets/abstract-dark.png' width={imgSize} height={imgSize}/>
+        <img src='./assets/eficode-light.png' width={imgSize} height={imgSize}/>
+        <img src='./assets/futurice-light.png' width={imgSize} height={imgSize}/>
+      </div>
       <SectionSubtitle color={Color.white} textAlign='center'>Supported by</SectionSubtitle>
+      <div style={{display: 'flex', justifyContent: 'center'}}>
+        <img src='./assets/idean-light.png' width={imgSize} height={imgSize}/>
+        <img src='./assets/smartly-dark.png' width={imgSize} height={imgSize}/>
+        <img src='./assets/intergalactico-dark.png' width={imgSize} height={imgSize}/>
+      </div>
     </Section>
   );
 }
